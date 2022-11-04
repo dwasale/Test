7a8c30980cd4d5d8f04777a683fa8d23c00df52e
Rudra <dwasale@gmail.com>
2022-11-04 13:18:56 +0000

    commit after adding username

--- src/Welcome.java
+++ src/Welcome.java
@@ -7,10 +7,14 @@ public class Welcome {
         address();
     }
 
-    public static void address(){
+    public static void address() {
         Scanner scanner = new Scanner(System.in);
+        System.out.print("\nEnter your full name: ");
+        String name = scanner.nextLine();
         System.out.println("Enter your full address: ");
         String address = scanner.nextLine();
+        
+        System.out.println(name);
         System.out.println(address);
     }
 }
