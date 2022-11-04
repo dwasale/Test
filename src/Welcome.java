476ee752e06e67e90ae6017373d0fcbe0eefecba
Rudra <dwasale@gmail.com>
2022-11-04 12:18:10 +0000

    Another simple commit

--- src/Welcome.java
+++ src/Welcome.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 public class Welcome {
 
     public static void main(String[] args) {
         System.out.println("Welcome to GITHUB....");
+        address();
+    }
+
+    public static void address(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your full address: ");
+        String address = scanner.nextLine();
+        System.out.println(address);
     }
 }
